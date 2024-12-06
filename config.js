57c4f59521f0d597499e9c88c0cb179ccc3d95b1
samith-dilshan <my.koyeb.ac@gmail.com>
2024-12-06 15:31:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tzrbzJF/3ba8644a4250.png",
-    ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BywLs0t/Any-Conv-com-15b52-1qaglhqizubs8tnecxh8ixg.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "I'm Alive Now",
 };
